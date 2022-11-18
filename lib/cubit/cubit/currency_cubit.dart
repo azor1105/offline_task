1dab92bc11c6f5729cfbc60d9ca3e000d5bcfbd5
azor1105 <azambekergashev1@gmail.com>
2022-11-18 16:14:23 +0500

    cubit had created

--- lib/cubit/cubit/currency_cubit.dart
+++ lib/cubit/cubit/currency_cubit.dart
@@ -2,11 +2,26 @@ import 'package:bloc/bloc.dart';
 import 'package:flutter/services.dart';
 import 'package:meta/meta.dart';
 import 'package:offline_task/data/models/nb_model.dart';
+import 'package:offline_task/data/repositories/currency_repository.dart';
 
 part 'currency_state.dart';
 
 class CurrencyCubit extends Cubit<CurrencyState> {
-  CurrencyCubit() : super(CurrencyInitial());
+  CurrencyCubit({required CurrencyRepository currencyRepository})
+      : _currencyRepository = currencyRepository,
+        super(CurrencyInitial());
 
-  
+  CurrencyRepository _currencyRepository;
+
+  Future<void> getCurrencies() async {
+    emit(CurrencyOnProgress());
+    try {
+      List<NbModel> currencies = await _currencyRepository.getCurrencies();
+      emit(CurrencyOnSuccess(currencies: currencies));
+    } catch (e) {
+      emit(
+        CurrencyOnError(error: e.toString()),
+      );
+    }
+  }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+import 'package:offline_task/cubit/cubit/currency_cubit.dart';
+import 'package:offline_task/data/api/api_client.dart';
+import 'package:offline_task/data/api/api_service.dart';
+import 'package:offline_task/data/repositories/currency_repository.dart';
 import 'package:offline_task/presentation/router/app_router.dart';
 import 'package:offline_task/utils/constants/route_names.dart';
 
 void main() {
-  runApp(const App());
+  runApp(BlocProvider(
+    create: (context) => CurrencyCubit(
+      currencyRepository: CurrencyRepository(
+        apiService: ApiService(
+          openApiClient: OpenApiClient(),
+        ),
+      ),
+    )..getCurrencies(),
+    child: const App(),
+  ));
 }
 
 class App extends StatelessWidget {
