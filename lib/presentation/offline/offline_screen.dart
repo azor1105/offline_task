0adf185dbaf9472a1431a65e425acc27d0d35224
Sodiqjon95 <sodiqdev95@gmil.com>
2022-11-18 18:04:22 +0500

    added connectivity logic

--- lib/presentation/offline/offline_screen.dart
+++ lib/presentation/offline/offline_screen.dart
@@ -10,56 +10,54 @@ class OfflineScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return WillPopScope(
-      onWillPop: () async => false,
-      child: Scaffold(
-        appBar: AppBar(
-          title: const Text("Offline page"),
-        ),
-        body: Column(
-          children: [
-            BlocListener<ConnectivityCubit, ConnectivityState>(
-              listener: (context, state) {
-                if (state.connectivityResult != ConnectivityResult.none) {
-                  Navigator.pop(context);
-                  context.read<CurrencyCubit>().getCurrencies();
-                }
-              },
-              child: const SizedBox(),
-            ),
-            Expanded(
-              child: FutureBuilder(
-                future: LocalDataBase.getAllCachedCurrency(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    return ListView(
-                      children: List.generate(
-                        snapshot.data!.length,
-                        (index) => Padding(
-                          padding: const EdgeInsets.symmetric(
-                              horizontal: 20, vertical: 10),
-                          child: Card(
-                            child: ListTile(
-                              title: Text(snapshot.data![index].title),
-                              subtitle: Text(snapshot.data![index].code),
-                              trailing: Text(snapshot.data![index].code),
-                            ),
+    return Scaffold(
+      appBar: AppBar(
+        leading: const SizedBox(),
+        title: const Text("Offline page"),
+        centerTitle: true,
+      ),
+      body: Column(
+        children: [
+          BlocListener<ConnectivityCubit, ConnectivityState>(
+            listener: (context, state) {
+              if (state.connectivityResult != ConnectivityResult.none) {
+                Navigator.pop(context);
+                context.read<CurrencyCubit>().getCurrencies();
+              }
+            },
+            child: const SizedBox(),
+          ),
+          Expanded(
+            child: FutureBuilder(
+              future: LocalDataBase.getAllCachedCurrency(),
+              builder: (context, snapshot) {
+                if (snapshot.hasData) {
+                  return ListView(
+                    children: List.generate(
+                      snapshot.data!.length,
+                      (index) => Padding(
+                        padding: const EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                        child: Card(
+                          child: ListTile(
+                            title: Text(snapshot.data![index].title),
+                            subtitle: Text(snapshot.data![index].code),
+                            trailing: Text(snapshot.data![index].code),
                           ),
                         ),
                       ),
-                    );
-                  } else if (snapshot.hasError) {
-                    return Center(child: Text(snapshot.error.toString()));
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
+                    ),
+                  );
+                } else if (snapshot.hasError) {
+                  return Center(child: Text(snapshot.error.toString()));
+                } else {
+                  return const Center(
+                    child: CircularProgressIndicator(),
+                  );
+                }
+              },
             ),
-          ],
-        ),
+          ),
+        ],
       ),
     );
   }
